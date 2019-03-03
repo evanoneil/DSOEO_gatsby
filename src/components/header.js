0f11a502761ca12bc212877d63e1cd2cbf178950
Evan ONeil <evan@evanoneil.net>
2019-03-03 13:26:05 -0600

    everything

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => <nav className="w-100 relative top-0-1 left-0-1 ph0-l-md pa5 flex items-baseline z-1">
+<div className="w-third ph3 w-100">
+  <h3 className="fw3"> {props.headerText}</h3>
+</div>
+<div className="w-two-thirds ph3 w-100 tr">
+
+</div>
+</nav>
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from "../components/header"
+
+const IndexPage = () => (
+<div style={{ color: `purple` }}>
+<Header headerText="Design Studio of Evan O'Neil" />   
+  <p className="f1">What a world.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+
+    </div>
+)
+
+export default IndexPage
