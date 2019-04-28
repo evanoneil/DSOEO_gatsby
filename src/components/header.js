1286bca676e2780a0d8aa8a54395def61ab331bf
Evan ONeil <evan@evanoneil.net>
2019-04-27 20:14:26 -0500

    4.27v3

--- src/components/header.js
+++ src/components/header.js
@@ -8,7 +8,7 @@ export default props => (
 
   
   <nav className="w-100 relative top-0-1 left-0-1 ph0-l-md pa5-l pa4 fl items-baseline z-1">
-    <div className="fl w-third pr3 w-100">
+    <div className="fl w-third pr1 w-100">
       <div className="fl"><a href="https://www.evanoneil.studio" className={styles.sitetitleheader}><h3 className="fw3">Design Studio of Evan O'Neil</h3></a></div>
     </div>
     <div className="fl w-two-thirds ph3 w-100 tr">
